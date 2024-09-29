9a0a96c30bbbefe68490678bb7e22fafc66d027d
Damiano Lozzi <damianolozzi1989@gmail.com>
2024-09-29 12:15:47 +0200

    refactored simple_logger

--- simple_logger/simple_logger.py
+++ simple_logger/simple_logger.py
@@ -5,7 +5,7 @@ import logging
 import json
 import os
 
-config = configparser.ConfigParser(interpolation=None)  # Disable interpolation
+config = configparser.ConfigParser(interpolation=None)
 config.read('config.ini')
 
 try:
@@ -16,7 +16,7 @@ try:
     COLORIZE_LOG = config.getboolean('LOGGING', 'LOG_COLORIZE', fallback=False)
     COLORIZE_JSON = config.getboolean('LOGGING', 'JSON_COLORIZE', fallback=False)
     LOG_LEVEL = config.get('LOGGING', 'LOG_LEVEL', fallback='DEBUG')
-    LOG_FORMAT = config.get('LOGGING', 'LOG_FORMAT', fallback='%(asctime)s | %(levelname)s\t|%(funcName)s |%(lineno)d\t| %(message)s')
+    LOG_FORMAT = config.get('LOGGING', 'LOG_FORMAT', fallback='%(asctime)s | %(levelname)s\t| %(filename)s \t|%(lineno)d\t| %(message)s')
     DATE_FORMAT = config.get('LOGGING', 'DATE_FORMAT', fallback='%Y-%m-%d %H:%M:%S')
 except configparser.NoSectionError:
     print('No LOGGING section found in config.ini. Using default values.')
@@ -27,19 +27,14 @@ except configparser.NoSectionError:
     COLORIZE_LOG = False
     COLORIZE_JSON = False
     LOG_LEVEL = 'DEBUG'
-    LOG_FORMAT = '%(asctime)s | %(levelname)s\t|%(funcName)s |%(lineno)d\t| %(message)s'
+    LOG_FORMAT = '%(asctime)s | %(levelname)s\t| %(filename)s \t|%(lineno)d\t| %(message)s'
     DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-    
-class Logger:
-    _instance = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(Logger, cls).__new__(cls)
-            cls._instance._initialize_logger(*args, **kwargs)
-        return cls._instance
-
-
+class Logger(logging.Logger):
+    def __init__(self):
+        super().__init__(__name__)
+        self._initialize_logger()
+        
     def _initialize_logger(self, log_directory=LOG_DIRECTORY,
                            log_filename=LOG_FILENAME,
                            json_log_filename=JSON_LOG_FILENAME,
@@ -49,8 +44,7 @@ class Logger:
                            log_level=LOG_LEVEL,
                            log_format=LOG_FORMAT,
                            date_format=DATE_FORMAT):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(self.get_log_level(log_level))
+        self.setLevel(self.get_log_level(log_level))
            
         file_logging = False if log_directory is None else True
 
@@ -61,8 +55,8 @@ class Logger:
             if not os.path.exists(log_directory):
                 os.makedirs(log_directory)
                 
-            if self.logger.hasHandlers():
-                self.logger.handlers.clear()
+            if self.hasHandlers():
+                self.handlers.clear()
 
             if colorize_log:
                 log_formatter = ColoredFormatter(log_format, datefmt=date_format)
@@ -70,7 +64,7 @@ class Logger:
                 log_formatter = logging.Formatter(log_format, datefmt=date_format)
             file_handler = RotatingFileHandler(log_file_path, maxBytes=5*1024*1024, backupCount=5)
             file_handler.setFormatter(log_formatter)
-            self.logger.addHandler(file_handler)
+            self.addHandler(file_handler)
             
             if colorize_json:
                 json_formatter = ColoredFormatter(log_format, datefmt=date_format)
@@ -78,7 +72,7 @@ class Logger:
                 json_formatter = logging.Formatter(log_format, datefmt=date_format)
             json_file_handler = RotatingFileHandler(json_log_file_path, maxBytes=5*1024*1024, backupCount=5)
             json_file_handler.setFormatter(json_formatter)
-            self.logger.addHandler(json_file_handler)
+            self.addHandler(json_file_handler)
 
         if colorize_console:
             console_formatter = ColoredFormatter(log_format, datefmt=date_format)
@@ -86,8 +80,8 @@ class Logger:
             console_formatter = logging.Formatter(log_format, datefmt=date_format)
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(console_formatter)
-        self.logger.addHandler(console_handler)
-
+        self.addHandler(console_handler)
+        
     def get_log_level(self, log_level):
         levels = {
             'DEBUG': logging.DEBUG,
@@ -96,43 +90,11 @@ class Logger:
             'ERROR': logging.ERROR,
             'CRITICAL': logging.CRITICAL
         }
-        return levels.get(log_level.upper(), logging.DEBUG)
+        return levels.get(log_level.upper(), logging.DEBUG)  
+    
 
-    def _get_stacklevel(self):
-        # Find the first frame that is not part of the logging module
-        frame = inspect.currentframe()
-        stacklevel = 1
-        log_levels = [
-        logging.getLevelName(logging.DEBUG),
-        logging.getLevelName(logging.INFO),
-        logging.getLevelName(logging.WARNING),
-        logging.getLevelName(logging.ERROR),
-        logging.getLevelName(logging.CRITICAL)
-        ]
-        while frame:
-            name=frame.f_globals['__name__']
-            print(f"Name: {name}")
-            if 'simple_logger' in name or any(level in name for level in log_levels):
-                frame = frame.f_back
-                stacklevel += 1
-            else:
-                break
-        return stacklevel
-
-    def error(self, msg, *args, **kwargs):
-        self.logger.error(msg, *args, **kwargs, stacklevel=self._get_stacklevel())
-
-    def info(self, msg, *args, **kwargs):
-        self.logger.info(msg, *args, **kwargs, stacklevel=self._get_stacklevel())
-
-    def warning(self, msg, *args, **kwargs):
-        self.logger.warning(msg, *args, **kwargs, stacklevel=self._get_stacklevel())
-
-    def debug(self, msg, *args, **kwargs):
-        self.logger.debug(msg, *args, **kwargs, stacklevel=self._get_stacklevel())
-
-    def critical(self, msg, *args, **kwargs):
-        self.logger.critical(msg, *args, **kwargs, stacklevel=self._get_stacklevel())
+        
+ 
 
 class ColoredFormatter(logging.Formatter):
     COLORS = {
